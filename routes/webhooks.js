8335559092dfe142b5026dd8f82dc0aa4a74db99
andriidski <andriidski@proton.me>
2017-07-20 12:15:08 -0400

    cleaned up some code and comments

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -17,6 +17,8 @@ const myTasksID = configurations.myTasksID;
 
 const webhookUri = process.env.WEBHOOK_URI;
 
+const debug = false;
+
 var userDataBase = [];
 
 var tunnelUrl = '';
@@ -34,7 +36,6 @@ if(testing) {
 // route handler for Slack's Event Notifications sent as POST requests to verify endpoint
 router.post('/events-auth', function(req,res) {
   const payload = req.body;
-
   if (payload.type === 'url_verification') {
     res.send(payload.challenge);
   }
@@ -87,13 +88,16 @@ router.post('/interactive-messages-endpoint', function(req,res) {
 
 router.post('/asana-webhook/:slug', function(req,res) {
   var asanaUserID = req.params.slug;
-  console.log("ASANA AUTH WEBHOOK HIT for user ID: " + asanaUserID);
   var hookAuthHeader = req.get("X-Hook-Secret");
-  console.log(hookAuthHeader);
+
+  if(debug) {
+    console.log("ASANA AUTH WEBHOOK HIT for user ID: " + asanaUserID);
+    console.log(hookAuthHeader);
+  }
+
   if(hookAuthHeader) {
     console.log("writing header");
     var response = createResponse(hookAuthHeader);
-    // console.log(response);
     res.setHeader('Content-Type', 'text');
     res.setHeader('X-Hook-Secret', hookAuthHeader);
     res.sendStatus(200);
@@ -102,11 +106,9 @@ router.post('/asana-webhook/:slug', function(req,res) {
     var signatureHeader = req.get("X-Hook-Signature");
     if(signatureHeader){
       console.log('got signature header');
-      // console.log(signatureHeader);
-      // console.log(req.body);
     }
     if(req.body.events.length > 0) {
-      // first fetch a list of users
+      // first fetch a list of users from Slack
       const options = {
         method: 'POST',
         uri: 'https://slack.com/api/users.list',
@@ -119,8 +121,7 @@ router.post('/asana-webhook/:slug', function(req,res) {
         }
       };
       request(options, function (error, response, body) {
-        // console.log(body.members);
-        // console.log(body.members);
+
         // fetching a correct user from the Asana data
         var currentUserFromData = {};
         userDataBase.forEach(function(databaseUserEntry) {
@@ -128,6 +129,7 @@ router.post('/asana-webhook/:slug', function(req,res) {
             currentUserFromData = databaseUserEntry;
           }
         });
+
         // fetching the correct user from Slack data
         var users = body.members;
         var currentUser = {};
@@ -136,21 +138,18 @@ router.post('/asana-webhook/:slug', function(req,res) {
           var slackUserName = (user.profile.first_name + ' ' + user.profile.last_name);
           if(currentUserFromData.name === slackUserName) {
             currentUser = user;
-            console.log("current user for event is: " + user.profile.first_name + " " + user.profile.last_name);
+            if(debug) {
+              console.log("current user for event is: " + user.profile.first_name + " " + user.profile.last_name);
+            }
           }
         });
 
-        // var currentUserID = process.env.CURRENT_USER_ID;
-        // users.forEach(function(user){
-        //   if(user.id === currentUserID) {
-        //     currentUser = user;
-        //     console.log("current user is: " + user.profile.first_name + " " + user.profile.last_name);
-        //   }
-        // });
-
         var events = cleanupEvents(req.body.events);
 
-        events.forEach(function(event){
+        console.log("EVENTS:");
+        console.log(events);
+
+        events.forEach(function(event) {
           if(event.type === 'task' && event.user) {
             var userID = asanaUserID;
             var headers = {
@@ -182,14 +181,13 @@ router.post('/asana-webhook/:slug', function(req,res) {
                     token: process.env.BOT_AUTH_TOKEN,
                     text: headerText,
                     attachments: JSON.stringify(jsonMessage),
-                    channel: currentUser.id, // set this to ID of person receiving the message
+                    channel: currentUser.id,
                     as_user: true
                   },
                   headers: {
                     'content-type': 'application/x-www-form-urlencoded'
                   }
                 };
-
                 request(options);
               } else if(event.action === 'added') {
                   console.log("A new Asana task has been added and assigned to " + dataObj.data.name + ".");
@@ -209,7 +207,7 @@ router.post('/asana-webhook/:slug', function(req,res) {
                       token: process.env.BOT_AUTH_TOKEN,
                       text: headerText,
                       attachments: JSON.stringify(jsonMessage),
-                      channel: currentUser.id, // set this to ID of person receiving the message
+                      channel: currentUser.id,
                       as_user: true
                     },
                     json: true,
@@ -217,7 +215,6 @@ router.post('/asana-webhook/:slug', function(req,res) {
                       'content-type': 'application/x-www-form-urlencoded'
                     }
                   };
-
                   request(options);
               } else if(event.action === 'deleted') {
                   console.log("An Asana task assigned to " + dataObj.data.name + " has been deleted.");
@@ -237,7 +234,7 @@ router.post('/asana-webhook/:slug', function(req,res) {
                       token: process.env.BOT_AUTH_TOKEN,
                       text: headerText,
                       attachments: JSON.stringify(jsonMessage),
-                      channel: currentUser.id, // set this to ID of person receiving the message
+                      channel: currentUser.id,
                       as_user: true
                     },
                     json: true,
@@ -245,7 +242,6 @@ router.post('/asana-webhook/:slug', function(req,res) {
                       'content-type': 'application/x-www-form-urlencoded'
                     }
                   };
-
                   request(options);
               }
             });
@@ -262,7 +258,6 @@ router.post('/asana-webhook/:slug', function(req,res) {
 function callback(error, response, body) {
   if (!error && response.statusCode == 201) {
     console.log("Successfully Authorized Asana Webhooks");
-    // console.log(body);
   } else {
     console.log('error');
     console.log(body);
@@ -291,10 +286,6 @@ router.get('/test', function(req, res, next) {
   const parsedData = JSON.parse(userData);
 
   userDataBase = parsedData.users;
-  console.log("----------------------------------------------------------------");
-  console.log("JSON of USERS:");
-  console.log(userDataBase);
-  console.log("----------------------------------------------------------------");
 
   userDataBase.forEach(function(user) {
     var taskListResourceID = user.taskListResourceID;
