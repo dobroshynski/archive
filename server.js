9c40419f4cb854fd1d0012c92bbb366cf107a48e
andriidski <andriidski@proton.me>
2017-07-21 11:30:08 -0400

    added basic server

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var webhooks = require('./src/webhooks');
+
+var server = express();
+
+server.use(logger('dev'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(cookieParser());
+
+server.use('/', webhooks);
+
+// catch 404 and forward to error handler
+server.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+server.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.sendStatus(err.status || 500);
+});
+
+module.exports = server;
