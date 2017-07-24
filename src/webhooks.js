00589efc51d17034b074ab998e7c59e4ce420f18
andriidski <andriidski@proton.me>
2017-07-24 09:11:07 -0400

    added receiving messages hook and sending back a response though API call

--- src/webhooks.js
+++ src/webhooks.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const request = require('request');
+
 router.get('/', function(req, res, next) {
   res.sendStatus(200);
 });
 
+// authenticate FB messenger webooks
 router.get('/messenger-webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === process.env.FB_VERIFY_TOKEN) {
     console.log("Validating webhook");
@@ -14,5 +17,79 @@ router.get('/messenger-webhook', function(req, res) {
     res.sendStatus(403);
   }
 });
+
+// listen for messages
+router.post('/messenger-webhook', function(req, res) {
+  var data = req.body;
+
+  if(data.object === 'page') {
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      entry.messaging.forEach(function(event) {
+        if(event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+    res.sendStatus(200);
+  }
+});
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+  sendAPICall(messageData);
+}
+
+function sendAPICall(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: process.env.FB_PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function(error, response, body) {
+    if(!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent message with id %s to recipient %s", messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
+}
+
+function receivedMessage(evnt) {
+  var senderID = evnt.sender.id;
+  var recipientID = evnt.recipient.id;
+  var timeOfMessage = evnt.timestamp;
+  var message = evnt.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if(messageText) {
+    sendTextMessage(senderID, messageText);
+  } else if(messageAttachments) {
+    console.log("received message with attachments");
+  }
+}
 
 module.exports = router;
